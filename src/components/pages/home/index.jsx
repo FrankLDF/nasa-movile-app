8f49f2166f689681fdebc8d0859952c0d08cf4db
FrankLDF <frankluisduarte00@gmail.com>
2023-11-15 17:31:37 -0400

    feat(home-page): add hom page component to the app

---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { format, sub } from "date-fns";
+
+import api from "../../../utils/api";
+import Header from "../../molecules/header";
+
+export default function Home() {
+  const [todaysImage, setTodaysImage] = useState();
+  const [lastFiveDaysImage, setLastFiveDaysImage] = useState();
+
+  useEffect(() => {
+    const lodTodaysImage = async () => {
+      try {
+        const todaysImageResponse = await api();
+        setTodaysImage(todaysImageResponse);
+      } catch (error) {
+        console.error(error);
+        setTodaysImage(undefined);
+      }
+    };
+
+    const loadLast5DaysImage = async () => {
+      try {
+        const date = new Date();
+        const todaysDate = format(date, "yyyy-MM-dd");
+        const fiveDaysAgoDate = format(sub(date, { days: 5 }), "yyyy-MM-dd");
+
+        const lastFiveDaysImageResponse = await api(
+          `&start_date=${fiveDaysAgoDate}&end_date=${todaysDate}`
+        );
+        setLastFiveDaysImage(lastFiveDaysImageResponse);
+      } catch (error) {
+        console.log(error);
+        setLastFiveDaysImage(undefined);
+      }
+    };
+
+    lodTodaysImage().catch(null);
+    loadLast5DaysImage().catch(null);
+  }, []);
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+});
